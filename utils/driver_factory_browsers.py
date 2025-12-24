d019a8cb132923e4b1eb40b846724244c7a6dc05
Farhat <f190886@mail.ru>
2025-12-24 12:35:56 +0400

    U9 Браузеры исправлены с учетом комментариев Ментора

--- utils/driver_factory_browsers.py
+++ utils/driver_factory_browsers.py
@@ -1,83 +1,76 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.ie.options import Options as IEOptions
-from selenium.webdriver.ie.service import Service as IEService
+
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.ie.service import Service as IEService
+
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager, IEDriverManager
 
 
 class DriverFactory:
-    @staticmethod
-    def create_driver(browser: str, remote: bool = False, remote_url: str = "http://localhost:4444"):
-        """
-        Создаёт WebDriver для указанного браузера
+    def __init__(self, remote_url: str = "http://localhost:4444"):
+        self.remote_url = remote_url
 
-        Args:
-            browser (str): 'chrome', 'firefox', 'edge', 'ie'
-            remote (bool): использовать Selenium Grid
-            remote_url (str): URL Grid (по умолчанию http://localhost:4444)
-        """
+    def create_driver(self, browser: str, remote: bool = False):
         browser = browser.lower()
-
         if remote:
-            return DriverFactory.create_remote_driver(browser, remote_url)
+            return self.create_remote_driver(browser)
         else:
-            return DriverFactory.create_local_driver(browser)
+            return self.create_local_driver(browser)
 
-    @staticmethod
-    def create_local_driver(browser: str):
+    def create_local_driver(self, browser: str):
         if browser == "chrome":
             service = ChromeService(ChromeDriverManager().install())
-            options = webdriver.ChromeOptions()
+            options = ChromeOptions()
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
             return webdriver.Chrome(service=service, options=options)
 
         elif browser == "firefox":
             service = FirefoxService(GeckoDriverManager().install())
-            options = webdriver.FirefoxOptions()
+            options = FirefoxOptions()
             return webdriver.Firefox(service=service, options=options)
 
         elif browser == "edge":
             service = EdgeService(EdgeChromiumDriverManager().install())
-            options = webdriver.EdgeOptions()
+            options = EdgeOptions()
             return webdriver.Edge(service=service, options=options)
 
         elif browser == "ie":
             service = IEService(IEDriverManager().install())
             options = IEOptions()
-            options.add_argument("--ignore-protected-mode-settings")
-            options.add_argument("--ignore-zoom-level")
-            options.add_argument("--disable-features=EnableEphemeralFlashPermission")
+            options.ignore_protected_mode_settings = True
+            options.ignore_zoom_level = True
             return webdriver.Ie(service=service, options=options)
 
         else:
-            raise ValueError(f"Unsupported browser: {browser}")
+            raise ValueError(f"Нерабочий browser: {browser}")
 
-    @staticmethod
-    def create_remote_driver(browser: str, remote_url: str):
-        from selenium.webdriver.chrome.options import Options as ChromeOptions
-        from selenium.webdriver.firefox.options import Options as FirefoxOptions
-        from selenium.webdriver.edge.options import Options as EdgeOptions
-        from selenium.webdriver.ie.options import Options as IEOptions
-
+    def create_remote_driver(self, browser: str):
         if browser == "chrome":
             options = ChromeOptions()
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--headless=new")
         elif browser == "firefox":
             options = FirefoxOptions()
+            options.add_argument("--headless")
         elif browser == "edge":
             options = EdgeOptions()
+            options.add_argument("--headless")
         elif browser == "ie":
             options = IEOptions()
-            options.add_argument("--ignore-protected-mode-settings")
-            options.add_argument("--ignore-zoom-level")
         else:
-            raise ValueError(f"Unsupported browser: {browser}")
+            raise ValueError(f"Нерабочий browser: {browser}")
 
         return webdriver.Remote(
-            command_executor=f"{remote_url}/wd/hub",
+            command_executor=f"{self.remote_url}/wd/hub",
             options=options
         )
